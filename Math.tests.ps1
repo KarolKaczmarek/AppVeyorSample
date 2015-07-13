c36e8291d997c7c95489c8b8eddfbab9430bc170
Karol Kaczmarek <KarolKaczmarek@users.noreply.github.com>
2015-07-13 16:58:38 -0700

    Adding pester tests

---
+++ Math.tests.ps1
@@ -0,0 +1,19 @@
+$homePath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Import Math.psm1 module
+Import-Module .\Math.psm1 -force
+
+Describe 'Math' {
+    Context 'multiply function' {
+        It 'should correctly multiply positive numbers' {
+            $result = multiply -first 4 -second 3
+            $result | Should Be 12
+        }
+
+        It 'should correctly multiply negative numbers' {
+            $result = multiply -first -5 -second 3
+            $result | Should Be -15
+        }
+    }
+    
+}
